35b16097a8ab7e9492286e3284e22e43c030ce7a
Aliarshia abdolahi <137824806+AliarshiaAbdolahi@users.noreply.github.com>
2023-07-05 19:10:06 +0330

    Create if the couple was.py

---
+++ Assignments 4/if the couple was.py
@@ -0,0 +1,15 @@
+number = float((input('Please enter your number: '))
+
+A = number % 2
+
+if number < 0:
+    print('D')
+
+elif A == 0 and number >= 6:
+    print('A')
+
+elif A == 0 and number <= 4:
+    print('B')
+
+else:
+    print('C')
