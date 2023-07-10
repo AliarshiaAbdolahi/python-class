b920bbdce776bc5d4c59dca0717186696102f904
Aliarshia abdolahi <137824806+AliarshiaAbdolahi@users.noreply.github.com>
2023-07-10 22:52:46 +0330

    Add files via upload

---
+++ assignment 3/converrt digits.py
@@ -0,0 +1,8 @@
+number = float(input("please enter a four digit number: "))
+Three_digitnumber = int(number//10)
+Two_digcitnumber = int(number//100)
+One_digitnumber = int(number//1000)
+
+print(Three_digitnumber)
+print(Two_digcitnumber)
+print(One_digitnumber)
---
+++ assignment 3/year_of_birth.py
@@ -0,0 +1,5 @@
+year_of_birth =int(input('please enter your year of birth: '))
+
+answer_1 = (1402 - year_of_birth)
+
+print(answer_1)
