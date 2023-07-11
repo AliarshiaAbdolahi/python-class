1ef7246c43eaba83790b309eb87aebba1a24b46a
Aliarshia abdolahi <137824806+AliarshiaAbdolahi@users.noreply.github.com>
2023-07-11 15:36:39 +0330

    Delete assignment 2 directory

--- assignment 2/Addition and subtraction.py
+++
@@ -1,8 +0,0 @@
-A = int(input('Please enter first number: '))
-B = int(input('Please enter second number: '))
-
-Answer_1 = (A + B)
-Answer_2 = (A - B)
-
-print('1-Addition: ',Answer_1)
-print('2-Subtraction: ',Answer_2)
