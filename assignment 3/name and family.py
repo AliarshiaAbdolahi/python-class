43367a9a5556d8e9db50b3842180bfd673bab7b5
Aliarshia abdolahi <137824806+AliarshiaAbdolahi@users.noreply.github.com>
2023-07-11 15:47:41 +0330

    Delete assignment 3 directory

--- assignment 3/name and family.py
+++
@@ -1,4 +0,0 @@
-Name = input('pealse enter your name: ')
-Family = input('please enter your family: ')
-
-print('you are:',Name,'_',Family)
--- assignment 3/year_of_birth.py
+++
@@ -1,5 +0,0 @@
-year_of_birth =int(input('please enter your year of birth: '))
-
-answer_1 = (1402 - year_of_birth)
-
-print(answer_1)
