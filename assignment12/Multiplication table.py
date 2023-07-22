7a788f8983c1fea6e0389e17b41ebd94efc40d18
Aliarshia abdolahi <137824806+AliarshiaAbdolahi@users.noreply.github.com>
2023-07-22 20:07:47 +0330

    Delete assignment12 directory

--- assignment12/Multiplication table.py
+++
@@ -1,7 +0,0 @@
-R = int(input("Please enter your (row): "))
-C = int(input("Please enter your (col): "))
-
-for i in range(1,C+1):
-    for j in range(1,R+1):
-        print(j * i,end="   ")
-    print()
--- assignment12/sum (n).py
+++
@@ -1,5 +0,0 @@
-def sum(number):
-    return f"{number}{number+number}{number+number+number}"
-
-number = int(input("Please enter your number: "))
-print("Your answer is:",sum(number))
