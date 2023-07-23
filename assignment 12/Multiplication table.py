fd0e4f0bd0e8029de5bb3aa3884d549b7812651b
Aliarshia abdolahi <137824806+AliarshiaAbdolahi@users.noreply.github.com>
2023-07-23 10:13:11 +0330

    Update Multiplication table.py

--- assignment 12/Multiplication table.py
+++ assignment 12/Multiplication table.py
@@ -1,7 +1,7 @@
-R = int(input("Please enter your (row): "))
-C = int(input("Please enter your (col): "))
+X = int(input("Please enter your (X): "))
+Y = int(input("Please enter your (Y): "))
 
-for i in range(1,C+1):
-    for j in range(1,R+1):
+for i in range(1,Y+1):
+    for j in range(1,X+1):
         print(j * i,end="   ")
     print()
