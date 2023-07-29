05efee611ce17c9bab59b8a7a57cf78132d60b9c
Aliarshia abdolahi <137824806+AliarshiaAbdolahi@users.noreply.github.com>
2023-07-30 00:53:07 +0330

    Add files via upload

---
+++ assignment 14/Class score.py
@@ -0,0 +1,12 @@
+class Score:
+    Student_name = None
+    Student_family = None
+    Score = None
+    Lesson_score = None
+
+    def Specifications():
+        pass
+    def Lesson():
+        pass
+    def start():
+        pass
