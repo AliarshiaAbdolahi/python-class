79ddf8ad5edff79b729759643ce177e3e4aa4e1e
Aliarshia abdolahi <137824806+AliarshiaAbdolahi@users.noreply.github.com>
2023-07-30 00:44:24 +0330

    Add files via upload

---
+++ assignment 14/Class teacher.py
@@ -0,0 +1,14 @@
+class teacher:
+    Name = None
+    Family = None
+    Age = None
+    Teaching_field = None
+    Teaching_school = None
+    Test = None
+
+    def Specifications():
+        pass
+    def Teaching():
+        pass
+    def taking_an_exam():
+        pass
