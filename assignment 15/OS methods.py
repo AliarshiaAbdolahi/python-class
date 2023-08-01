3068a7c641dd0bf923541869ef75f15d14e61a24
Aliarshia abdolahi <137824806+AliarshiaAbdolahi@users.noreply.github.com>
2023-08-01 10:24:06 +0330

    Add files via upload

---
+++ assignment 15/OS methods.py
@@ -0,0 +1,15 @@
+import os
+
+method_1 = os.getcwd()
+# cwd = os.getcwd
+# print("You are in the:",cwd)
+# method_2 = os.chdir
+
+method_2 = os.mkdir("")
+# os.mkdir("mydir")
+
+method_3 = os.chdir("")
+# os.chdir("mydir")
+
+method_4 = os.rename
+# os.rename("test","new name")
