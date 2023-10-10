0e03216f614c5e669bbbff716e3d7aa727996a2b
Aliarshia abdolahi <137824806+AliarshiaAbdolahi@users.noreply.github.com>
2023-10-10 17:43:08 +0330

    Update voice.py

--- assignment 16/voice.py
+++ assignment 16/voice.py
@@ -1,8 +1,19 @@
 from gtts import gTTS
 
-def convert_text_to_speach(text):
+def convert_text_to_speach(text,name):
     audio = gTTS(text)
-    audio.save("voice.mp3")
+    audio.save(f"assignment 16/{name}.mp3")
 
-text = input("Please enter your text: ")
-convert_text_to_speach(text)
+text_1 = input("Please enter your text 1: ")
+text_2 = input("Please enter your text 2: ")
+text_3 = input("Please enter your text 3: ")
+name = input("Please enter name of voice: ")
+
+if len(text_1) > len(text_2) and len(text_1) > len(text_3):
+    convert_text_to_speach(text_1,name)
+
+elif len(text_2) > len(text_1) and len(text_2) > len(text_3):
+    convert_text_to_speach(text_2,name)
+
+elif len(text_3) > len(text_1) and len(text_3) > len(text_2):
+    convert_text_to_speach(text_3,name)
